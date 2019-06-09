1ff9a1414d22b87fb94d8e7c483284336d922740
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-06-09 21:58:27 +0300

    add overlay and transistion menu

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -1,1 +1,40 @@
 // Start Code JavaScript Here
+
+// Select Item From DOM Elements
+const humbruger = document.querySelector('.humbruger');
+const brand = document.querySelector('.brand');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav-links');
+const navItem = document.querySelectorAll('.nav-item');
+
+// show menu 
+
+let showMenu = false;
+
+humbruger.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+    if (!showMenu) {
+
+        humbruger.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        brand.classList.add('show');
+        navItem.forEach(item => {
+            item.classList.add('show')
+        })
+        showMenu = true;
+    } else {
+
+        humbruger.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        brand.classList.remove('show');
+        navItem.forEach(item => {
+            item.classList.remove('show');
+        })
+        showMenu = false;
+    }
+}
+
